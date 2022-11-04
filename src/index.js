9e312aa18107436dfe4e3742b43092f8b02b4fc8
Tcumulus <maarten.laureyssen@gmail.com>
2022-11-04 20:05:57 +0100

    Google Analytics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
+import ReactGA from "react-ga4";
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+ReactGA.initialize("G-KNVG93M203")
+ReactGA.send("pageview")
 
 ReactDOM.render(
   <React.StrictMode>
